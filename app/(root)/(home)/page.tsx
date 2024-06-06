91e1b18c9e26c7486d39a61c6c37072f91fa92b9
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-07 03:25:51 +0530

    issue solved

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,16 +1,24 @@
-'use client'
+"use client";
 import MeetingTypeList from "@/components/MeetingTypeList";
 import React, { useState } from "react";
 import ReactDatePicker from "react-datepicker";
 
 const Home = () => {
-  
+  const now = new Date();
+
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
+
   const [values, setValues] = useState({
     dateTime: new Date(),
     description: "",
     link: "",
   });
-  
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
@@ -18,20 +26,21 @@ const Home = () => {
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[270px] rounded py-2 px-2 text-base font-bold">
             Keep connecting...
-
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl "><ReactDatePicker
-              selected={values.dateTime}           
-              onChange={(date) => setValues({ ...values, dateTime: date! })}
-              showTimeSelect
-              timeFormat="HH:mm"
-              //timeIntervals={15}
-              timeCaption="time"
-              dateFormat="MMMM d, yyyy h:mm aa"
-              className="w-full rounded-[10px] glassmorphism p-1 focus:outline-none "
-            /></h1>
-            
+            <h1 className="text-4xl font-extrabold lg:text-7xl ">
+              <ReactDatePicker
+                selected={values.dateTime}
+                onChange={(date) => setValues({ ...values, dateTime: date! })}                
+                readOnly={true}
+                showTimeSelect
+                timeFormat="HH:mm"
+                //timeIntervals={15}
+                timeCaption="time"
+                dateFormat="MMMM d, h:mm aa, yyyy"
+                className="w-full rounded-[10px] glassmorphism p-1 focus:outline-none "
+              />
+            </h1>
           </div>
         </div>
       </div>
